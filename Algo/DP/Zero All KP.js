50733d2b2f488e7f8c57efd2809d99777dd49e6f
ArrmenLee <wachemanstonphoenix@gmail.com>
2020-12-12 22:54:31 +0800

    (DP)[M] Add test case of 0/1 KP

--- Algo/DP/Zero All KP.js
+++ Algo/DP/Zero All KP.js
@@ -34,7 +34,12 @@ const zeroAllKP = (items, W) => {
   return c[numberOfItems-1][W];
 };
 
-const items = [{weight: 4, value: 45}, {weight: 5, value: 57}, {weight: 2, value: 22.5}, {weight: 1, value: 11}, {weight: 6, value: 67}];
-const W = 8;
+let items = [{weight: 4, value: 45}, {weight: 5, value: 57}, {weight: 2, value: 22.5}, {weight: 1, value: 11}, {weight: 6, value: 67}];
+let W = 8;
 // http://www2.lssh.tp.edu.tw/~hlf/class-1/lang-c/kp.htm
+console.log('0/1 KP problem\nitems =', items, ', W =', W, '\nres = ', zeroAllKP(items, W));
+
+items = [{weight: 10, value: 60}, {weight: 20, value: 100}, {weight: 30, value: 120}];
+W = 50;
+// https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/?ref=lbp
 console.log('0/1 KP problem\nitems =', items, ', W =', W, '\nres = ', zeroAllKP(items, W));
