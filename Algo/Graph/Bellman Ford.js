e372f49d4c59dcf9d21ffa26a1832bef5aa81857
ArrmenLee <wachemanstonphoenix@gmail.com>
2020-12-27 13:21:24 +0800

    *(Graph)[A] Bellman ford

--- Algo/Graph/Bellman Ford.js
+++ Algo/Graph/Bellman Ford.js
@@ -1,23 +1,23 @@
 // Chapter 24
 // Chapter -
 
-// Bellman Ford =
+// Bellman Ford = VE
 // input: G = G[i]: list of {e: edge destination, w: weight}, s: start vertex
 // output: minimum cost to each vertex
-const INF = Infinity // Number.MAX_VALUE;
+const INF = Infinity;
 
 const BellmanFord = (G, s) => {
   const V = G.length;
   const dist = new Array(V);
-  dist.fill(Number.MAX_VALUE);
+  dist.fill(INF);
   dist[s] = 0;
 
   const relax = (p) => {
-    for (let k = 0; k < V; k++)
+    for (let k = 0; k < V; k++) // <- 應檢查有無邊再 relax，才能使 iteration = O(E)
       dist[p] = Math.min(dist[p], dist[k] + G[k][p]);
   };
 
-  for (let i = 0; i < V-1; i++) {
+  for (let i = 0; i < V-1; i++) { // V
     for (let j = 0; j < V; j++) {
       relax(j);
     }
